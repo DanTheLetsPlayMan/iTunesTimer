42cc7407423d60fba557824a1bcd2a013652901a
zuerrdan <zuerrdan@students.zhaw.ch>
2016-05-05 09:17:43 +0200

    Added SoundCloud (open in Safari) to list of supported apps

--- iTunes Timer/AppDelegate.swift
+++ iTunes Timer/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  iTunes Timer
+//  iTunes and Media Timer
 //
-//  Created by Daniel Zürrer on 02.06.15.
-//  Copyright (c) 2015 Daniel Zuerrer. All rights reserved.
+//  Created by Daniel Zuerrer on 02.06.15.
+//  Copyright (c) 2012 Daniel Zuerrer. All rights reserved.
 //
 
 import Cocoa
@@ -13,22 +13,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     let menuText = "iTimer"
     var error: NSDictionary?
-    
-    var timerTime = 60 * 30;
-    var counter = 0;
-    var timer = NSTimer()
-    
+
     var commandScripts = [String: String]()
     var activeCommand = ""
     
     let iTunesString = "iTunes"
     let VLCString = "VLC"
     let YouTubeString = "YouTube"
+    let SoundCloudString = "SoundCloud"
     
     
     @IBOutlet weak var timeDisplay: NSMenuItem!
     @IBOutlet weak var statusMenu: NSMenu!
     @IBOutlet weak var commandDisplay: NSMenuItem!
+
+    //times
+
+    var availableTimesInMinutes = [15,30,45,60]
+
+    var timerTime = 60 * 30
+    var counter = 0;
+    var timer = NSTimer()
+
+    @IBOutlet weak var sixtyMin: NSMenuItem!
+    @IBOutlet weak var fortyFiveMin: NSMenuItem!
+    @IBOutlet weak var thirtyMin: NSMenuItem!
+    @IBOutlet weak var fifteenMin: NSMenuItem!
+
     
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1) // NSVariableStatusItemLength
@@ -42,7 +53,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func startClicked(sender: AnyObject) {
         timer.invalidate();
         counter = timerTime;
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("changeTitle"), userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: #selector(AppDelegate.changeTitle), userInfo: nil, repeats: true)
     }
     @IBAction func resetClicked(sender: AnyObject) {
         counter = timerTime;
@@ -51,37 +62,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     @IBAction func sixtyClicked(sender: AnyObject) {
         resetClicked(sender);
-        changeTimerTime(60)
+        changeTimerTime(availableTimesInMinutes[3])
     }
     @IBAction func fortyFiveClicked(sender: AnyObject) {
         resetClicked(sender);
-        changeTimerTime(45)
+        changeTimerTime(availableTimesInMinutes[2])
     }
     @IBAction func thirtyClicked(sender: AnyObject) {
         resetClicked(sender);
-        changeTimerTime(30)
+        changeTimerTime(availableTimesInMinutes[1])
     }
     @IBAction func fifteenClicked(sender: AnyObject) {
         resetClicked(sender);
-        changeTimerTime(15)
+        changeTimerTime(availableTimesInMinutes[0])
     }
     
     
     
     @IBAction func iTunesClicked(sender: AnyObject) {
-        activeCommand = iTunesString
-        commandDisplay.title = iTunesString
+        renewActiveCommand(iTunesString);
     }
     @IBAction func VLCClicked(sender: AnyObject) {
-        activeCommand = VLCString
-        commandDisplay.title = VLCString
+        renewActiveCommand(VLCString);
     }
     @IBAction func SafariClicked(sender: AnyObject) {
-        activeCommand = YouTubeString
-        commandDisplay.title = YouTubeString
+        renewActiveCommand(YouTubeString);
     }
     
-    
+    @IBAction func SoundCloudClicked(sender: AnyObject) {
+        renewActiveCommand(SoundCloudString);
+    }
+
+    func renewActiveCommand(newActiveCommand: String){
+        activeCommand = newActiveCommand;
+        commandDisplay.title = activeCommand
+
+    }
+
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -91,16 +108,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         addCommandScipts()
         activeCommand = iTunesString
         commandDisplay.title = activeCommand
+
+        // set times in menu display
+        initialiseMenuTimesDisplay()
         
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-    
+
+    // update menu bar title with current remaining time
     func changeTitle() {
         let minutes = counter / 60;
-        let seconds = counter-- % 60;
+        let seconds = counter % 60;
+        counter -= 1;
         
         
         if (minutes == 0 && seconds == 0) {
@@ -113,7 +135,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem.title = "Remaining: " + createTimeString(counter)
     }
-    
+
+    // pause playback of selected application
     func pausePlayback() {
         if let scriptObject = NSAppleScript(source: commandScripts[activeCommand]!) {
             if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
@@ -126,7 +149,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         print("paused " + activeCommand)
     }
-    
+
+    // creating a time string (min:sec) from a time in seconds
     func createTimeString(timeInSeconds: Int) -> String {
         
         let minutes = timeInSeconds / 60;
@@ -140,13 +164,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         return String(minutes) + ":" + secondsString;
     }
-    
+
+    // update the remaining time of the timer
     func changeTimerTime(newTime: Int) {
         timerTime = 60 * newTime;
         timeDisplay.title = createTimeString(timerTime)
 
     }
-    
+
+    // add apple scripts to pause various apps
     func addCommandScipts() {
         commandScripts[iTunesString] = "tell application \"iTunes\" to pause"
         commandScripts[VLCString] = "tell application \"System Events\"" + "\n" +
@@ -157,6 +183,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             "tell application \"Safari\" to activate" + "\n" +
             "key code 40" + "\n" +
             "end tell"
+        commandScripts[SoundCloudString] = "tell application \"System Events\"" + "\n" +
+            "tell application \"Safari\" to activate" + "\n" +
+            "keystroke space" + "\n" +
+            "end tell"
+    }
+
+    // initialize menu time list with correct values
+    func initialiseMenuTimesDisplay(){
+        fifteenMin.title = createTimeString(availableTimesInMinutes[0] * 60)
+        thirtyMin.title = createTimeString(availableTimesInMinutes[1] * 60)
+        fortyFiveMin.title = createTimeString(availableTimesInMinutes[2] * 60)
+        sixtyMin.title = createTimeString(availableTimesInMinutes[3] * 60)
     }
     
     
